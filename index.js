bf4e9af7d4d782605044f3e0d2bccae26c4d3767
Jalen Mnene <mnenejalen72@gmail.com>
2024-04-03 11:36:23 +0300

    Finished javascript functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(symbol = "*") {
+    return function inner(adjective = "precious") {
+    return `You are ${symbol}${adjective.toLowerCase()}${symbol}!`;
+    };
+  }
